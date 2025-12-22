32e79598950eebbaa3c99ae5305484deef8bbdfb
jingxu-i-xiaozhou <jingxushi13@gmail.com>
2025-12-22 21:43:24 +0800

    新增 ai 预测服务

---
+++ src/test/java/com/ncwu/predictionservice/PredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncwu.predictionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
